2d33963a71cb6cd1c9ae47f7b661523d4a645aa5
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-06-10 19:15:01 -0300

    Configuring travis.ci

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+image_name=fabiohbarbosa/linux-mint
+image_version=17
+
+docker build -t ${image_name}:${image_version} .
+docker run --rm -i -t ${image_name}:${image_version} lsb_release -ca
